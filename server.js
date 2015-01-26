01f6ad46592b6837e9b9b16f6941956f04253a03
DadaMonad <kevin.jahns@rwth-aachen.de>
2015-01-26 23:33:36 +0000

    release to yjs

---
+++ server.js
@@ -0,0 +1,30 @@
+
+var Y = require('yjs');
+Y.XMPP = require('y-connectors').XMPP;
+
+var connector = new Y.XMPP("meme-together-qq", {
+  host: "yatta.ninja",
+  user: "dmonad@users.yatta.ninja",
+  password: process.argv[2],
+});
+
+connector.debug = true
+
+var y = new Y(connector);
+var feed = y.val("feed",[],"mutable").val("feed");
+
+/*
+ Do not allow a feed size greater that 10
+*/
+
+var feed_size = 10;
+
+feed.observe(function(){
+  // whenever something changed, check if the feedt size exceeds 10
+  var length = feed.val().length;
+  if(length > feed_size){
+    feed.delete(feed_size,length-feed_size)
+  }
+})
+console.log("listening!")
+
