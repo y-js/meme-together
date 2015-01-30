eec302e6c0358d3124ed3e6c6cdb1c778fdfaa66
DadaMonad <kevin.jahns@rwth-aachen.de>
2015-01-30 11:23:35 +0000

    Updated the connectors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+/*
+ * Using Yjs on nodejs is not very different
+ * from using it in the browser. It is *just another client*!
+ * in some 
+ */
 
-var Y = require('yjs');
-Y.XMPP = require('y-connectors').XMPP;
+var Y = require('../yjs/build/node/y');
+Y.XMPP = require('../y-connectors/build/node/index').XMPP;
 
-var connector = new Y.XMPP("meme-together-qq", {
-  host: "yatta.ninja",
+var connector = new Y.XMPP({
+  host: "yatta.ninja", // You _must_ specify the host, when using it with nodejs (this is our testing server)
   user: "dmonad@users.yatta.ninja",
   password: process.argv[2],
-});
+}).join("meme-together");
 
 connector.debug = true
 
@@ -14,13 +19,15 @@ var y = new Y(connector);
 var feed = y.val("feed",[],"mutable").val("feed");
 
 /*
- Do not allow a feed size greater that 10
+ * The server has the function to not allow a feed size greater that 15.
+ * When a greater number is reached, it will automatically delete the
+ * oldest posts.
 */
 
-var feed_size = 10;
+var feed_size = 15;
 
 feed.observe(function(){
-  // whenever something changed, check if the feedt size exceeds 10
+  // whenever something changed, check if the feed size exceeds 15
   var length = feed.val().length;
   if(length > feed_size){
     feed.delete(feed_size,length-feed_size)
